b96fcbcd59a32be2ceb1ce37c0ef6dc4a91e9abd
vaydheesh <lokendras1998@gmail.com>
2019-07-27 21:40:18 +0530

    python/setup.py build for windows

--- python/setup.py
+++ python/setup.py
@@ -3,12 +3,21 @@
 '''
 Setup for SWIG Python bindings for lex-tools
 '''
+import platform
 from os import path
 from distutils.core import Extension, setup
 from distutils.command.build import build
+import distutils.cygwinccompiler
 
 
 class CustomBuild(build):
+    if platform.system() == 'Windows':
+        distutils.cygwinccompiler.get_msvcr = lambda: []
+
+        def initialize_options(self):
+            super(CustomBuild, self).initialize_options()
+            self.compiler = 'mingw32'
+
     sub_commands = [
         ('build_ext', build.has_ext_modules),
         ('build_py', build.has_pure_modules),
@@ -34,9 +43,21 @@ def get_include_dirs():
 lextools_module = Extension(
     name='_lextools',
     sources=get_sources(),
-    swig_opts=['-c++', '-I..', '-Wall']+'-I/usr/local/include/lttoolbox-3.5 -I/usr/local/lib/lttoolbox-3.5/include -I/usr/include/libxml2 '.split(),
-    include_dirs=get_include_dirs(),
-    library_dirs=['/usr/include/libxml2', '/usr/local/lib'],
+    swig_opts=['-c++', '-I..', '-Wall']+\
+        '-I../../lttoolbox/lttoolbox -I../../lttoolbox '.split(),
+    include_dirs=[
+    '..',
+    '../../lttoolbox',
+    '../../lttoolbox/lttoolbox',
+    '../../externallibs/win64/libxml2-2.9.3-win32-x86_64/include/libxml2',
+    '../utf8',
+    '../../externallibs/win64/iconv-1.14-win32-x86_64/include',
+    ],
+    library_dirs= [
+    '../../externallibs/win64/libxml2-2.9.3-win32-x86_64/lib',
+    'C:/Python37/libs',
+    '../../externallibs/win64/local-build',
+    ],
     extra_compile_args='-Wall -Wextra -g -O2 -std=c++2a'.split(),
     extra_link_args=['-lxml2', '-llttoolbox3'],
 )
